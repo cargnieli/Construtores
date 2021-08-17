45b22a37904fe468096373f03d7a6638a09ed848
Walter Donizety Cargnieli <cargnieli@yahoo.com>
2021-08-17 10:49:54 -0300

    Initial commit

---
+++ src/Pessoa.java
@@ -0,0 +1,15 @@
+public class Pessoa {
+
+    String nome;
+    int idade;
+
+    Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    Pessoa(String nome, int idade) {
+        this(nome);
+        this.idade = idade;
+
+    }
+}
